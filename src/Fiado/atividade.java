727a534e4887cb9f85a210f9dcb3382af772b80e
Guilherme <guilhermeerosa@hotmail.com>
2020-08-26 15:20:55 -0300

    Commit inicial

---
+++ src/Fiado/atividade.java
@@ -0,0 +1,40 @@
+package Fiado;
+
+import java.util.Scanner;
+
+public class atividade {
+
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.print("Informe o numero de fiados: ");
+		Integer f = Integer.valueOf(scanner.nextLine());
+		System.out.print("Informe o nome do cliente: ");
+		String nome = scanner.nextLine();
+		
+		int[] fiados = new int[f];
+		
+		int i = 0;
+		while (i < fiados.length) {
+			System.out.print("Informe o valor da compra: ");
+			fiados[i] = Integer.valueOf(scanner.nextLine());
+			i++;
+		}
+		
+		int total = somaWhile(fiados);
+		
+		System.out.println("Cliente " + nome + " deve: " + total);
+
+	}
+	
+	public static int somaWhile(int[] fiados) {
+		int i = 0;
+		int total = 0;
+		while (i < fiados.length) {
+			total += fiados[i];
+			i++;
+		}
+		return total;
+	}
+}
