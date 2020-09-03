3439d1b9badf2e699191817c1a9cb052efd7ec64
Guilherme Rosa <59878498+guilhermeerosa@users.noreply.github.com>
2020-09-03 11:13:22 -0300

    Commit aula 5

--- src/Fiado/Main.java
+++ src/Fiado/Main.java
@@ -7,25 +7,23 @@ public class Main {
 	public static void main(String[] args) {
 
 		Consumidor consumidor = new Consumidor();
-		consumidor.nome = "Paula";
-		System.out.println("Consumidor: " + consumidor.nome);
 		
 		Scanner scanner = new Scanner(System.in);
 		System.out.print("Informe o nome do cliente: ");
-		String nome = scanner.nextLine();
+		consumidor.nome = scanner.nextLine();
 		System.out.print("Informe o valor da compra: ");
 		consumidor.fiados[0] = Integer.valueOf(scanner.nextLine());
 		
 		int total = somaWhile(consumidor.fiados);
 		
 		if (total > 100 ) {
-			System.out.println("Cliente " + nome + " deve: " + total + " - ganhou brinde");
+			System.out.println("Cliente " + consumidor.nome + " deve: " + total + " - ganhou brinde");
 		}
 		else {
-			System.out.println("Cliente " + nome + " deve: " + total);
+			System.out.println("Cliente " + consumidor.nome + " deve: " + total);
 		}
 		
-		System.out.println("Cliente " + nome + " deve:" + total);
+		System.out.println("Cliente " + consumidor.nome + " deve:" + total);
 
 	}
 	
