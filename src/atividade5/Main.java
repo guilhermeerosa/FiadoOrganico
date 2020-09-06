a4e082c25d451c7170193882d423b5c74d5bd53c
Guilherme Rosa <59878498+guilhermeerosa@users.noreply.github.com>
2020-09-06 15:21:43 -0300

    Update Main.java

--- src/atividade5/Main.java
+++ src/atividade5/Main.java
@@ -17,45 +17,23 @@ public class Main {
 		System.out.print("Informe o numero de compras: ");
 		Integer compras = Integer.valueOf(scanner.nextLine());
 		
-		System.out.print("Informe o valor da compra: ");
-		int fiado = Integer.valueOf(scanner.nextLine());
-		consumidor.registrarFiado(fiado);
+		int c = 0;
+		while(c < compras) {
+			System.out.print("Informe o valor da compra: ");
+			int fiado = Integer.valueOf(scanner.nextLine());
+			consumidor.registrarFiado(fiado);
+			c++;
+		}
 		
 		int total = consumidor.getFiado();
 		
 		if (total > 100 ) {
-			System.out.println("Cliente " + consumidor.getNome() + " deve: " + total + " - ganhou brinde");
+			System.out.println("Cliente " + consumidor.getNome() + " deve: " + total + " reais - ganhou brinde");
 		}
 		else {
-			System.out.println("Cliente " + consumidor.getNome() + " deve: " + total);
+			System.out.println("Cliente " + consumidor.getNome() + " deve: " + total + " reais");
 		}
 
-	}
-	
-	public static int somaWhile(int[] fiados) {
-		int i = 0;
-		int total = 0;
-		while (i < fiados.length) {
-			total += fiados[i];
-			i++;
-		}
-		return total;
-	}
-	
-	public static int somaFor(int[] fiados) {
-		int total = 0;
-		for (int i = 0; i < fiados.length; i++) {
-			total += fiados[i];
-		}
-		return total;
-	}
-	
-	public static int somaForeach(int[] fiados) {
-		int total = 0;
-		for (int fiado : fiados) {
-			total += fiado;
-		}
-		return total;
 	}
 
 }
