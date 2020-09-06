34451b5429dc15702869d5523ef2064f54b65b86
Guilherme Rosa <59878498+guilhermeerosa@users.noreply.github.com>
2020-09-06 15:12:22 -0300

    Commit

---
+++ src/atividade5/Consumidor.java
@@ -0,0 +1,38 @@
+package atividade5;
+
+public class Consumidor {
+	
+	//Atributos
+	private String nome;
+	private int[] fiados;
+	private int posicao = 0;
+	
+	//Construtor
+	public Consumidor (String nome) {
+		this.nome = nome;
+		this.fiados = new int [10];
+	}
+	
+	//GETTER e SETTER
+	public String getNome() {
+		return this.nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public void registrarFiado(int valor) {
+		this.fiados[posicao] = valor;
+		posicao++;
+	}
+	
+	public int getFiado() {
+		int total = 0;
+		for (int fiado : fiados) {
+			total += fiado;
+		}
+		return total;
+	}
+
+}
---
+++ src/atividade5/Main.java
@@ -0,0 +1,61 @@
+package atividade5;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		
+		//consumidor.fiados = new int[10];
+		
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Informe o nome do cliente: ");
+		String nome = scanner.nextLine();
+		Consumidor consumidor = new Consumidor(nome);
+		
+		System.out.print("Informe o numero de compras: ");
+		Integer compras = Integer.valueOf(scanner.nextLine());
+		
+		System.out.print("Informe o valor da compra: ");
+		int fiado = Integer.valueOf(scanner.nextLine());
+		consumidor.registrarFiado(fiado);
+		
+		int total = consumidor.getFiado();
+		
+		if (total > 100 ) {
+			System.out.println("Cliente " + consumidor.getNome() + " deve: " + total + " - ganhou brinde");
+		}
+		else {
+			System.out.println("Cliente " + consumidor.getNome() + " deve: " + total);
+		}
+
+	}
+	
+	public static int somaWhile(int[] fiados) {
+		int i = 0;
+		int total = 0;
+		while (i < fiados.length) {
+			total += fiados[i];
+			i++;
+		}
+		return total;
+	}
+	
+	public static int somaFor(int[] fiados) {
+		int total = 0;
+		for (int i = 0; i < fiados.length; i++) {
+			total += fiados[i];
+		}
+		return total;
+	}
+	
+	public static int somaForeach(int[] fiados) {
+		int total = 0;
+		for (int fiado : fiados) {
+			total += fiado;
+		}
+		return total;
+	}
+
+}
